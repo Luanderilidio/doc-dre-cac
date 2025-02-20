0cba4462c90e2ea9ec5aea0f1e7d89640b2e4c7b
Luander <luanderilidio174@gmail.com>
2025-02-20 17:17:21 -0300

    altera status e adiciona stepper

--- src/components/CardDocument.tsx
+++ src/components/CardDocument.tsx
@@ -10,10 +10,14 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Step,
+  StepLabel,
+  Stepper,
   TextField,
 } from "@mui/material";
 import moment from "moment";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import RefreshIcon from "@mui/icons-material/Refresh";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import { useBoolean } from "react-hooks-shareable";
 import { useState } from "react";
@@ -43,6 +47,15 @@ export interface CardDocumentProps {
   comprovanteEndereco?: string;
   status: string;
   funcionario?: string;
+  motivo?: string;
+}
+
+interface StatusOrderParams {
+  id: string;
+  action: string;
+  status: string;
+  employee: string;
+  reason: string;
 }
 
 export default function CardDocument({
@@ -67,25 +80,25 @@ export default function CardDocument({
   comprovanteEndereco,
   status,
   funcionario,
+  motivo,
 }: CardDocumentProps) {
-
   const apiUrl = import.meta.env.VITE_BACK_END_URL as string;
 
   const dataSolicitada = moment(timestamp);
   const hoje = moment();
+
   const betwenDays = hoje.diff(dataSolicitada, "days");
 
   const [isView, openView, closeView, toggleView] = useBoolean(false);
 
-  const [isViewAccept, openViewAccept, closeViewAccept, toggleViewAccept] =
-    useBoolean(false);
-  const [isViewDenied, openViewDenied, closeViewDenied, toggleViewDenied] =
+  const [isLoanding, openLoanding, closeLoanding, toggleLoanding] =
     useBoolean(false);
 
-    
-
-  const [employee, setEmployee] = useState<string>("");
-  const [reason, setReason] = useState<string>("");
+  const [employee, setEmployee] = useState<string>(funcionario || "");
+  const [reason, setReason] = useState<string>(motivo || "");
+  const [statusOrder, setStatusOrder] = useState<string>(
+    status || "no_service"
+  );
 
   // Função para calcular a mensagem de diferença de dias
   const qtdDaysMessage = (): string => {
@@ -97,71 +110,26 @@ export default function CardDocument({
       return `há ${betwenDays} dias atrás`;
     }
   };
-
-  // Função para alterar o status para "Negado"
-  const handleStatusDenied = async () => {
+  const handleStatusOrder = async (
+    status: string,
+    employee: string,
+    reason: string
+  ) => {
     try {
-      const response = await api.get(apiUrl, {
-        params: {
-          action: "denied",
-          id: id + 1,
-          employee,
-          reason,
-        },
-      });
-      toggleViewDenied();
+      const params: StatusOrderParams = {
+        action: "status",
+        status: status,
+        employee: employee,
+        reason: reason,
+        id: id + 1,
+      };
+
+      console.log(id, status, employee, reason);
+      const response = await api.get(apiUrl, { params });
+
       toggleView();
+      closeLoanding();
       console.log(response);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  // Função para alterar o status para "Finalizado"
-  const handleStatusFinished = async () => {
-    try {
-      const response = await api.get(apiUrl, {
-        params: {
-          action: "finished",
-          id: id + 1,
-        },
-      });
-      toggleView();
-      console.log(response);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  // Função para alterar o status para "Em Atendimento"
-  const handleStatusInService = async () => {
-    try {
-      const response = await api.get(apiUrl, {
-        params: {
-          action: "in_service",
-          id: id + 1,
-          employee,
-        },
-      });
-      toggleViewAccept();
-      closeView();
-      console.log(response);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  // Função para alterar o status para "Liberado"
-  const handleStatusDelivery = async () => {
-    try {
-      const response = await api.get(apiUrl, {
-        params: {
-          action: "delivery",
-          id: id + 1,
-        },
-      });
-      console.log(response);
-      toggleView();
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -181,10 +149,23 @@ export default function CardDocument({
     return input.replace(/[^0-9]/g, "");
   }
 
-  // Exemplo de uso
   const formattedNumber = formatToNumbers(telefoneContato);
 
-  // const formattedNumber = telefoneContato.replace(/\D/g, '');
+  const getStatusOrder = (statusOrder: string) => {
+    switch (statusOrder) {
+      case "no_service":
+        return 0;
+      case "in_service":
+        return 1;
+      case "delivery":
+        return 2;
+      case "finished":
+        return 3;
+      default:
+        console.log("Ação desconhecida:");
+        return null;
+    }
+  };
 
   return (
     <div className="col-span-1 font-Montserrat flex flex-col border rounded-lg p-4 shadow-black/30 drop-shadow-sm m-2">
@@ -390,8 +371,115 @@ export default function CardDocument({
               )}/preview`}
               allow="autoplay"
             />
-            <div className="grid grid-cols-2 gap-2 py-2">
-              {status === "no_service" && (
+            <Divider className="col-span-2 !mt-4">
+              Alterar Status da Solicitação
+            </Divider>
+            {statusOrder !== "denied" ? (
+              <>
+                <Stepper
+                  className="my-7"
+                  activeStep={getStatusOrder(statusOrder) || 0}
+                  alternativeLabel
+                >
+                  <Step key={0}>
+                    <StepLabel>
+                      Documento sem <br /> Atendimento
+                    </StepLabel>
+                  </Step>
+                  <Step key={1}>
+                    <StepLabel>
+                      Documento em <br /> Atendimento
+                    </StepLabel>
+                  </Step>
+                  <Step key={2}>
+                    <StepLabel>
+                      Documento <br /> Liberado
+                    </StepLabel>
+                  </Step>
+                  <Step key={3}>
+                    <StepLabel>
+                      Documento <br /> Finalizado{" "}
+                    </StepLabel>
+                  </Step>
+                </Stepper>
+              </>
+            ) : (
+              <>
+                <Stepper
+                  className="my-7"
+                  activeStep={getStatusOrder(statusOrder) || 0}
+                  alternativeLabel
+                >
+                  <Step key={0}>
+                    <StepLabel>
+                      Solicitação <br /> Negada
+                    </StepLabel>
+                  </Step>
+                </Stepper>
+              </>
+            )}
+
+            <div className="grid grid-cols-6 gap-2 py-2">
+              <FormControl variant="outlined" className="col-span-3">
+                <InputLabel>Funcionário</InputLabel>
+                <Select
+                  fullWidth
+                  className="col-span-3"
+                  value={employee}
+                  onChange={(event) => setEmployee(event.target.value)}
+                  label="Funcionário"
+                >
+                  <MenuItem value="Graciane">Graciane</MenuItem>
+                  <MenuItem value="Carmelito">Carmelito</MenuItem>
+                  <MenuItem value="Luander">Luander</MenuItem>
+                </Select>
+              </FormControl>
+
+              <FormControl variant="outlined" className="col-span-3">
+                <InputLabel>Status</InputLabel>
+                <Select
+                  fullWidth
+                  value={statusOrder}
+                  onChange={(event) => setStatusOrder(event.target.value)}
+                  label="Status"
+                >
+                  <MenuItem value="in_service">Em Atendimento</MenuItem>
+                  <MenuItem value="denied">Negado</MenuItem>
+                  <MenuItem value="delivery">Liberado</MenuItem>
+                  <MenuItem value="finished">Finalizado</MenuItem>
+                </Select>
+              </FormControl>
+              {statusOrder === "denied" && (
+                <TextField
+                  className="col-span-6"
+                  label="Motivo"
+                  multiline
+                  rows={2}
+                  maxRows={4}
+                  value={reason}
+                  onChange={(event) => setReason(event.target.value)}
+                  defaultValue="escreva o motivo aqui"
+                  variant="outlined"
+                />
+              )}
+              <Button
+                onClick={() => {
+                  openLoanding();
+                  handleStatusOrder(statusOrder, employee, reason);
+                }}
+                variant="contained"
+                className="col-span-6"
+              >
+                {isLoanding === true ? (
+                  <>
+                    Alterando <RefreshIcon className="animate-spin" />
+                  </>
+                ) : (
+                  <>Alterar</>
+                )}
+              </Button>
+              {/* {status === "no_service" && (
+
                 <>
                   <Button
                     onClick={() => {
@@ -545,7 +633,7 @@ export default function CardDocument({
                     Negado
                   </Button>
                 </>
-              )}
+              )} */}
             </div>
           </DialogContentText>
         </DialogContent>
