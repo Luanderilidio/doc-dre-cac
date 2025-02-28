f8b8d15a1e4ada166beaf2d922af297ae9d76793
Luander <luanderilidio174@gmail.com>
2025-02-28 12:45:41 -0300

    retira states e container 3

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from "../pages/Admin";
 import Home from "../pages/Home";
-import Contatos from "../pages/Contatos";
 import Solicitacao from "../pages/Solicitacao";
 
 const AppRoutes = () => {
@@ -12,8 +11,6 @@ const AppRoutes = () => {
           <Route path={`/`} element={<Home />} />
           <Route path={`/arquivo`} element={<Admin />} />
           <Route path={`/solicitacao/:id`} element={<Solicitacao />} />
-
-          <Route path={`/contatos`} element={<Contatos />} />
           <Route path="*" element={<>No Match</>} />
         </Routes>
       </BrowserRouter>
