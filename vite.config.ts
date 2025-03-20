3fb5c99497ef9b79d6ce54e6d4672a60a9c3e3b5
luanderilidio <luanderilidio174@gmail.com>
2025-03-20 09:21:46 -0300

    retira esLint

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
