db506ab0ed8c5fa34b6578acefe7e82c4c1cd267
Luander <luanderilidio174@gmail.com>
2025-03-27 16:45:46 -0300

    adiciona formulário novamente

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { scan } from "react-scan"; 
+import { createRoot } from "react-dom/client";
 import App from './App.tsx'
 import './index.css'
-
-
-// scan({
-//   enabled: true,
-// });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
